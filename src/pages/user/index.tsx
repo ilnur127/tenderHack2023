c32e64a5db75e31e3b11e2c854c8654570b120c5
ilnur127 <ilnurgareev8@gmail.com>
2023-10-22 15:36:07 +0300

    fix

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,5 +1,26 @@
+import { useEffect } from "react"
+// import Select from "react-select/dist/declarations/src/Select"
+import { toast } from "react-toastify"
+
 const UserPage = (): JSX.Element => {
-  return <div>User</div>
+  useEffect(() => {
+    toast.error('Ошибка')
+  }, [])
+  return <div>
+    <div>
+      {/* <Select
+        value={selectedFilterData?.category}
+        onChange={(option) => changeFilterData('category', option)}
+        options={classesList}
+        getOptionLabel={(option: TClasses) => option.className}
+        getOptionValue={(option: TClasses) => option.classEnum}
+        placeholder="Выберете класс"
+      />
+      <Select />
+      <Select /> */}
+      Пользователь
+    </div>
+  </div>
 }
 
 export default UserPage
